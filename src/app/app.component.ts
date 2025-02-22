ec1a36d6d8868845ae0a6dbcafca091c865372c6
Samgrass <samuelegrassetti01@gmail.com>
2025-02-22 17:37:56 +0100

    Initialize app with header, footer and main pages components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./component/footer/footer.component";
+import { HeaderComponent } from "./component/header/header.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NewMovieComponent } from './new-movie/new-movie.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'aggiungi-film',
+    component: NewMovieComponent
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent
+  }
+];
