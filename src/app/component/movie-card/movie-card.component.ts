3adac057fbd72885f0592d5e0c7e6db71db0e487
Samgrass <samuelegrassetti01@gmail.com>
2025-02-25 13:54:24 +0100

    add functions to check if the movie is in the favourites and to emit the movie status to be added or removed

--- src/app/component/movie-card/movie-card.component.ts
+++ src/app/component/movie-card/movie-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Movie } from '../../model/movies';
 
 @Component({
@@ -11,4 +11,23 @@ export class MovieCardComponent {
 
   // ricevo l'array di film da visualizzare da new-movie.component.ts
   @Input() allMovies: Movie[] | null = null;
+
+  // emetto l'evento per aggiungere o rimuovere i film dai preferiti
+  @Output() movieSelected = new EventEmitter<Movie>();
+  @Output() movieRemoved = new EventEmitter<Movie>();
+
+  // controllo se il film è già nei preferiti
+  isMovieInLocalStorage(movieId: number): boolean {
+    const favMovies = JSON.parse(localStorage.getItem('favMovies') || '[]');
+    return favMovies.some((storedMovie: Movie) => storedMovie.id === movieId);
+  }
+
+  // metodo per aggiungere o rimuovere i film dai preferiti
+  selectMovie(movie: Movie): void {
+    if (this.isMovieInLocalStorage(movie.id)) {
+      this.movieRemoved.emit(movie);
+    } else {
+      this.movieSelected.emit(movie);
+    }
+  }
 }
